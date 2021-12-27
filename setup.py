45e4969706b6e71c7ed6c2e6570bfcc3d7933445
Alex-like <62363788+Alex-like@users.noreply.github.com>
2021-12-27 17:47:05 +0300

    Add url in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='0.1.0',    
     description='Useful functions for statistics.',
     author='Alex Shchelochkov',
+    author_email='email@email.com',
+    url='https://github.com/Alex-like/modul-alex1.git',
     packages=['module_alex1'],
     install_requires=['numpy>=1.9.0'],
 
